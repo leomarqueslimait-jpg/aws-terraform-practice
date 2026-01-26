5a3327f8c26d29d146babddbe14bfc3376003895
leomarqueslimait-jpg <leomarqueslimait@gmail.com>
2026-01-26 12:01:48 -0600

    deleted files

--- 1 - Dynamic_AZ/locals.tf
+++ 1 - Dynamic_AZ/locals.tf
@@ -1,4 +1,3 @@
 locals {
   project = "count_expression"
-  ami_id = "ubuntu"
 }
--- Resources/compute.tf
+++
@@ -1,9 +0,0 @@
-
-
-resource "aws_instance" "web_servers" {
-  count = 3
-  ami = local.ami_id
-  instance_type = var.server_config.instance_type
-    
-
-}
